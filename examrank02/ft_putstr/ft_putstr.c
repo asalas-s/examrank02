ea5ddb891456fa2feecd4175a75bc96b42aeeeeb
elanonimotranquilo <elanonimotranquilo@gmail.com>
2023-01-19 01:20:22 +0100

    Add files via upload

---
+++ examrank02/ft_putstr/ft_putstr.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_putstr(char *str)
+{
+	int	len;
+
+	len = ft_strlen(str);
+	write(1, str, len);
+}
